a83f16374aad6e3713c94f6f1619bff489616e44
PedroCavallaro <pedrolemecavallaro@gmail.com>
2023-08-03 19:00:57 -0300

    fix: fixed alert

--- src/app/lib/swall.ts
+++ src/app/lib/swall.ts
@@ -1,7 +1,9 @@
 import Swal from "sweetalert2";
 
 export const swal = Swal.mixin({
-    position: "top",
+    position: "top-end",
+    toast: true,
+    showConfirmButton: false,
     icon: "success",
     timer: 2000,
     text: "Mensagem enviada!",
